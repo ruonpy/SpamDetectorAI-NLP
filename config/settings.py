6b934678fe09314fb9d99ca2adeeaf426b237e7c
Onur ICLI <onuricli.py@gmail.com>
2025-08-03 19:59:03 +0300

    feat: refactor settings into a Config class better structure

--- config/settings.py
+++ config/settings.py
@@ -1,20 +1,19 @@
 from pathlib import Path
 
-BASE_DIR=Path(__file__).parent.parent
-DATA_DIR=BASE_DIR/"data"
-MODEL_DIR=BASE_DIR/"models"/"saved"
-
 """
-MODEL_NAME:
-    - cased:
-        * It is important that difference between Onur and onur
-    - multilingual:
-        * I want it to detect languages other than English.
-        * If you want to detect just English use bert-base-cased
-"""
-
-MODEL_NAME="bert-base-multilingual-cased"
-MAX_LENGTH=128
-BATCH_SIZE=16
-LEARNING_RATE=2e-5
-EPOCHS=3
+    MODEL_NAME:
+        - cased:
+            * It is important that difference between Onur and onur
+        - multilingual:
+            * I want it to detect languages other than English.
+            * If you want to detect just English use bert-base-cased
+    """
+class Config:
+    BASE_DIR=Path(__file__).parent.parent
+    DATA_PATH="trspam.csv"
+    DATA_DIR=BASE_DIR/"data"
+    MODEL_DIR=BASE_DIR/"models"/"saved"
+    MAX_LENGTH=128
+    BATCH_SIZE=16
+    LEARNING_RATE=2e-5
+    EPOCHS=3
